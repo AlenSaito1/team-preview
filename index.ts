e44da295f71510a5b9cbd43b052eadd1f206bce7
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-06-09 16:22:07 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -122,7 +122,7 @@ export async function partyScreen(data: Party<PokemonSet>): Promise<Buffer> {
 		ctx.fillStyle = drawdata[i].color;
 		ctx.fillText(name !== '' ? name : species, drawdata[i].species.x, drawdata[i].species.y);
 		ctx.fillText('Lv. ' + level, drawdata[i].lvl.x, drawdata[i].lvl.y);
-		let icon = await loadImage(`https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/${data[i].species.toLowerCase()}.png`);
+		let icon = await loadImage(`https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/${data[i].species.toLowerCase()}.png`).catch(() => loadImage(`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/${data[i].species.toLowerCase()}.png`)).catch(() => loadImage(`https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen7x/regular/${data[i].species.toLowerCase()}.png`))
 		ctx.drawImage(icon, drawdata[i].icon.x, drawdata[i].icon.y, icon.width*1.5, icon.height*1.5);
 		if (gender) gender === 'M' ? ctx.drawImage(male, drawdata[i].gender.x, drawdata[i].gender.y) : ctx.drawImage(female, drawdata[i].gender.x, drawdata[i].gender.y);
 
